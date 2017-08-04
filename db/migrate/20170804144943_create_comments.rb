676b6ab7c091a65d6231a07e6749e7ac2c507b7f
N <limepassion@gmail.com>
2017-08-04 18:04:14 +0300

    polymorphic edit migration

--- db/migrate/20170804144943_create_comments.rb
+++ db/migrate/20170804144943_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.string :content
-
+      t.references :commentable, polymorphic: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,11 @@ ActiveRecord::Schema.define(version: 20170804145036) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
+    t.string "commentable_type"
+    t.integer "commentable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
   create_table "places", force: :cascade do |t|

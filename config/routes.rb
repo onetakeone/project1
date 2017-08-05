1d40102ea40fa092a6008b143ab535a1179e3779
N <limepassion@gmail.com>
2017-08-05 11:19:10 +0300

    correct routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  resources :places
-  resources :comments
-  resources :posts
+  concern :commentable do
+    resources :comments
+  end
+  
+  resources :places, concerns: :commentable
+  resources :posts, concerns: :commentable
+ 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
